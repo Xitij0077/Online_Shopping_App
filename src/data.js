711a999d521f8262bcfce03dcdd501752c59eef0
xitij0077 <xitijgudekar0077@gmail.com>
2023-12-07 21:17:57 +0530

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,98 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "./images/Home1.PNG",
+    title: "SUMMER SALE",
+    desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+
+  {
+    id: 2,
+    img: "./images/Home2.PNG",
+    title: "SUMMER SALE",
+    desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+
+  {
+    id: 3,
+    img: "./images/Home3.png",
+    title: "SUMMER SALE",
+    desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpg",
+    title: "SHIRT STYLE!",
+  },
+
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpg",
+    title: "LOUNGWEAR LOVE",
+  },
+
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+    title: "LIGHT JACKETS",
+  },
+
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+    title: "NEW DRESSES",
+  },
+
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+    title: "THE PERFECT JEANS!",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+
+  {
+    id: 8,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+  },
+];
