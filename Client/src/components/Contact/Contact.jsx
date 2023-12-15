c563af3991b4e5775282a44f19e1293a4f5bf652
xitij0077 <xitijgudekar0077@gmail.com>
2023-12-16 00:02:12 +0530

    Contact Page created

---
+++ Client/src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Contact.scss";
+
+import * as MuiIcons from "@mui/icons-material";
+
+const { Facebook, Instagram, Twitter, Google, Pinterest } = MuiIcons;
+import "./Contact.scss";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="wrapper">
+        <span>BE IN TOUCH WITH US:</span>
+        <div className="mail">
+          <input type="text" placeholder="Enter your email-ID..." />
+          <button>JOIN US</button>
+        </div>
+        <div className="icon">
+          <Facebook />
+          <Instagram />
+          <Twitter />
+          <Google />
+          <Pinterest />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
